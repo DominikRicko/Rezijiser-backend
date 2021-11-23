e1a5f5d40663ee8364b9b70f0adbe4b7d258cf23
Dominik Ričko <dominik.ricko@gmail.com>
2021-11-23 18:16:25 +0100

    initial commit

---
+++ src/main/java/hr/foka/rezijiser/RezijiserApplication.java
@@ -0,0 +1,13 @@
+package hr.foka.rezijiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezijiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezijiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/foka/rezijiser/RezijiserApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.foka.rezijiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezijiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
